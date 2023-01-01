70a12613759a3edc9f321535d6fb03b37a14dfa5
Gabriel da Silva <falcon_gabriel@outlook.com>
2023-01-01 16:26:07 -0300

    adicionando e corrigindo funcionalidades

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const conteiner_seconds = document.querySelector('.seconds');
 const conteiner_milliseconds = document.querySelector('.milliseconds');
 const conteiner_minutes = document.querySelector('.minutes');
 const start_button = document.querySelector('.start');
-const stop_button = document.querySelector('.stop');
+const pause_button = document.querySelector('.pause');
+const clear_button = document.querySelector('.clear');
 
 const timer = {
     second: 0, 
@@ -25,13 +26,19 @@ const timer = {
     },
     start: () => {
         clearInterval(timer.interval);
-        timer.millisecond = 0;
-        timer.second = 0;
-        timer.minute = 0;
         timer.interval = setInterval(timer.timing, 10);
     },
     stop: () => {
         clearInterval(timer.interval)
+    },
+    clear: () => {
+        clearInterval(timer.interval);
+        timer.millisecond = 0;
+        timer.second = 0;
+        timer.minute = 0;
+        conteiner_milliseconds.innerHTML = timer.update(timer.millisecond / 10)
+        conteiner_seconds.innerHTML = timer.update(timer.second);
+        conteiner_minutes.innerHTML = timer.update(timer.minute);
     },
     update: (time) => {
         if (time >= 10) {
@@ -43,4 +50,5 @@ const timer = {
 
 
 start_button.addEventListener('click', timer.start);
-stop_button.addEventListener('click', timer.stop);
+pause_button.addEventListener('click', timer.stop);
+clear_button.addEventListener('click', timer.clear)
